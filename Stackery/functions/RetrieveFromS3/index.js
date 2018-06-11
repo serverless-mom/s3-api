f92103af56363253c4c197d55c83bc48f4aa016a
Toby Fee <TobyFee@gmail.com>
2018-06-11 12:33:32 -0700

    retrieve text file

--- Stackery/functions/RetrieveFromS3/index.js
+++ Stackery/functions/RetrieveFromS3/index.js
@@ -1,3 +1,7 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+
 module.exports = async request => {
   // Log the request to the console.
   console.dir(request);
@@ -5,19 +9,29 @@ module.exports = async request => {
   // Get the message sent in the endpoint path parameter
   let message = request.pathParameters.fileId;
 
-  // Build a response.
-  let responseBody = `
-    <h4>You sent the following message:</h4>
-    <p>${message}</p>
-  `;
 
-  let response = {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "text/html"
-    },
-    body: responseBody
+  let params = {
+    Key: `${fileId}.txt`,
+    Bucket: ports[0][0].bucket
   };
 
-  return response;
+  try {
+    let s3Return = await s3.getObject(params);
+    console.dir({
+      status: success,
+      return: s3Return
+    })
+  } catch (error) {
+    console.error(err.message, event);
+  }
+
+  // let response = {
+  //   statusCode: 200,
+  //   headers: {
+  //     "Content-Type": "text/html"
+  //   },
+  //   body: responseBody
+  // };
+  //
+  // return response;
 };
