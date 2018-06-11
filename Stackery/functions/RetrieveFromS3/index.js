61f8390715789769a4c3747514e900cac002f12c
Toby Fee <TobyFee@gmail.com>
2018-06-11 13:22:58 -0700

    linting fixes

--- Stackery/functions/RetrieveFromS3/index.js
+++ Stackery/functions/RetrieveFromS3/index.js
@@ -24,7 +24,7 @@ module.exports = async request => {
       return: s3Return
     })
   } catch (error) {
-    console.error(error.message);
+    console.error(error.message, event);
   }
 
   // let response = {
